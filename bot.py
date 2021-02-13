84466fa40aa8c2d9c09fff27c3655232abc2862e
ida.levison.3316 <ida.levison.3316@student.lu.se>
2021-02-13 12:21:57 +0100

    Log in feature added Decline notifications feature added

--- bot.py
+++ bot.py
@@ -10,4 +10,30 @@ sleep(3)
 acceptcookies = browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]")
 acceptcookies.click()
 
-#browser.close()
+
+sleep(2)
+
+username_input = browser.find_element_by_css_selector("input[name='username']")
+password_input = browser.find_element_by_css_selector("input[name='password']")
+
+userInfoFile = open("userinfo.txt", "r")
+username = userInfoFile.readline()
+password = userInfoFile.readline()
+
+print(username)
+print(password)
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
+login_button.click()
+
+sleep(6)
+
+notifications = browser.find_element_by_css_selector('button.aOOlW:nth-child(2)')
+notifications.click()
+print("clicked")
+
+sleep(7)
+browser.close()
