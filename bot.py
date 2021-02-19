ba1e90e263f30fbca0e90574262bd5ae78948d38
felix.noble_andreasson.2686 <fe2686no-s@student.lu.se>
2021-02-19 14:33:05 +0100

    Story: UserInfo done and Refactored. Also added gitignore

--- bot.py
+++ bot.py
@@ -2,46 +2,66 @@ from time import sleep
 from selenium import webdriver
 import os
 
-configFilePath = "config.ini"
+class UserLogin:
 
-def validateConfigFile(filePath):
-    if os.path.isfile(filePath):
-        if not (os.stat(filePath).st_size == 0):
-            return True
-        else: 
+    def __init__(self):
+        self.configFilePath = "config.ini"
+        self.usern = ""
+        self.passw = ""
+
+    def validateConfigFile(self, filePath):
+        if os.path.isfile(filePath):
+            if not (os.stat(filePath).st_size == 0):
+                return True
+            else:
+                return False
+        else:
             return False
-    else:
-        return False
 
-def getUserInfo():
-    usern = ""
-    passw = ""
-    if validateConfigFile(configFilePath): 
-        userInfoFile = open(configFilePath, "r")
-        lines = userInfoFile.readlines()
-        for line in lines:
-            if ("user" in line):
-                usern = line.split('=')[1]
-                print(usern)
-            if ("password" in line):
-                passw = line.split('=')[1]
-                print(passw)
+    def userMissing(self):
+        print(
+            "No user info found, please enter credentials to create new configuration file.")
+        self.createNewUser()
 
-        print("Credentials import successful.")
-        print("Logging in using credentials for user:" + usern)
-    else:
-        print("No user info found, please enter credentials to create new configuration file.")
-        print("username: ", end ='')
-        usern = str(input()) 
-        print("Enter password: ", end = '')
-        passw = str(input())
-        userInfoFile = open((configFilePath),"w+")
-        userInfoFile.writelines(["username=" + usern, "\n","password="+ passw])
+    def createNewUser(self):
+
+        print("Enter username: ", end='')
+        self.usern = str(input())
+        print("Enter password: ", end='')
+        self.passw = str(input())
+
+        userInfoFile = open((self.configFilePath), "w+")
+        userInfoFile.writelines(
+            ["username=" + self.usern, "\n", "password=" + self.passw])
         userInfoFile.close()
-    return [usern, passw]
+        print("Credentials successfully stored in " + self.configFilePath)
+
+    def changeConfigPath(self, newPath):
+        configFilePath = newPath
+
+    def getUserInfo(self):
+        
+        if self.validateConfigFile(self.configFilePath):
+            userInfoFile = open(self.configFilePath, "r")
+            lines = userInfoFile.readlines()
+            for line in lines:
+                if ("user" in line):
+                    self.usern = line.split('=')[1]
+                if ("password" in line):
+                    self.passw = line.split('=')[1]
+
+            if (not (self.passw and self.usern)):
+                self.userMissing()
+            else:
+                print("Credentials import successful.")
+                print("Logging in using credentials for user: " + self.usern)
+        else:
+            self.userMissing()
+        return [self.usern, self.passw]
 
 
-userInfo = getUserInfo()
+userLogin = UserLogin()
+userInfo = userLogin.getUserInfo()
 username = userInfo[0]
 password = userInfo[1]
 
@@ -52,7 +72,8 @@ browser.get("https://www.instagram.com/")
 
 sleep(3)
 
-acceptcookies = browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]")
+acceptcookies = browser.find_element_by_xpath(
+    "/html/body/div[2]/div/div/div/div[2]/button[1]")
 acceptcookies.click()
 
 
@@ -69,11 +90,10 @@ login_button.click()
 
 sleep(6)
 
-notifications = browser.find_element_by_css_selector('button.aOOlW:nth-child(2)')
+notifications = browser.find_element_by_css_selector(
+    'button.aOOlW:nth-child(2)')
 notifications.click()
 print("clicked")
 
 sleep(7)
 browser.close()
-
-    
